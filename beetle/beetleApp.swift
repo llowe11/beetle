2064b70c3e5ded61ce8940ea8a4bddcf806ccbf3
llowe11 <vladislavkim566@gmail.com>
2025-02-28 19:02:56 +0500

    INIT: initialize a new project

---
+++ beetle/beetleApp.swift
@@ -0,0 +1,17 @@
+//
+//  beetleApp.swift
+//  beetle
+//
+//  Created by Vladislav Kim on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct beetleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
