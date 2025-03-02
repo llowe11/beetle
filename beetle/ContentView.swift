ec8c651c58760822a54fb8b980e8c95679322aa6
llowe11 <vladislavkim566@gmail.com>
2025-03-02 14:49:12 +0500

    FEAT: edit homepage

--- beetle/ContentView.swift
+++ beetle/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Home Page !")
+            Text("CultuRise!")
         }
         .padding()
     }
